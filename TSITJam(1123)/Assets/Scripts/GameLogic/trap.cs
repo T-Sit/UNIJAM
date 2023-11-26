567ae2f2d436e9bb07602630efc39670dee1a271
Daniil Kirkota <95646707+1Kirik3@users.noreply.github.com>
2023-11-26 16:29:40 +0300

    Add full Level 2

--- TSITJam(1123)/Assets/Scripts/GameLogic/trap.cs
+++ TSITJam(1123)/Assets/Scripts/GameLogic/trap.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 
 public class trap : MonoBehaviour
@@ -5,6 +6,7 @@ public class trap : MonoBehaviour
     private void OnTriggerEnter(Collider other){
         if(other.CompareTag("Player")){
             other.GetComponent<PlayerControl>().StopFootsteps();
+            other.GetComponent<Transform>().transform.DOKill();
             Destroy(other.gameObject);
             AudioManager.Instance.PlayOneShot(FMODEvents.Instance.PlayerDeath);
         }
